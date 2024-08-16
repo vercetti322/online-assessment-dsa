43764b141f0860dce7e82a7244ba3065381ebbe4
Jatin Jindal <116031920+vercetti322@users.noreply.github.com>
2024-08-16 09:21:45 +0000

    Oracle: q2 done

--- Oracle/Q2/solution.cpp
+++ Oracle/Q2/solution.cpp
@@ -2,20 +2,78 @@
 
 using namespace std;
 
-// dsu approach
-vector<int> getTheGroups(int n, vector<string>& qType, vector<int>& s1, vector<int>& s2) {
-    int q = qType.size();
-    vector<int> ans;
-    vector<set<int>> grps;
-    int grp_size = 0;
+class DSU {
+    vector<int> rank, parent, size;
+    public:
+        DSU(int n) {
+            rank.resize(n + 1, 0);
+            parent.resize(n + 1);
+            size.resize(n + 1);
+            for (int i = 0; i <= n; i++) {
+                parent[i] = i;
+                size[i] = 1;
+            }
+        }
 
-    for (int i = 0; i < q; i++) {
-        if (qType[i] == "Friend") {
+        // with path compression
+        int findUltPar(int node) {
+            if (node == parent[node])
+                return node;
+            else
+                return parent[node] = findUltPar(parent[node]);
+        }
+
+        // union by rank
+        void unionByRank(int u, int v) {
+            int ulp_u = findUltPar(u);
+            int ulp_v = findUltPar(v);
             
+            if (ulp_u == ulp_v) return;
+            if (rank[ulp_u] < rank[ulp_v])
+                parent[ulp_u] = ulp_v;
+            else if (rank[ulp_v] > rank[ulp_v])
+                parent[ulp_v] = ulp_u;
+            else {
+                parent[ulp_v] = ulp_u;
+                rank[ulp_u]++;
+            } 
+        }
+
+        // union by size
+        void unionBySize(int u, int v) {
+            int ulp_u = findUltPar(u);
+            int ulp_v = findUltPar(v);
+            
+            if (ulp_u == ulp_v) return;
+            if (size[ulp_u] < size[ulp_v]) {
+                parent[ulp_u] = ulp_v;
+                size[ulp_v] += size[ulp_u];
+            }
+            else {
+                parent[ulp_v] = ulp_u;
+                size[ulp_u] += size[ulp_v];
+            }
+        }
+
+        int getSize(int node) {
+            int ulp_node = findUltPar(node);
+            return size[ulp_node];
+        }
+};
+
+vector<int> getTheGroups(int n, vector<string>& qt, vector<int>& s1, vector<int>& s2) {
+    DSU dsu(n);
+    vector<int> ans;
+    for (int i = 0; i < qt.size(); i++) {
+        if (qt[i] == "Friend") {
+            dsu.unionBySize(s1[i], s2[i]);
         } else {
-            ans.push_back(grp_size);
+            int total_size = dsu.getSize(s1[i]) + dsu.getSize(s2[i]);
+            ans.push_back(total_size);
         }
     }
+
+    return ans;
 }
 
 
